13f3acc22aef8aa2af0ec70ec6da70e32189cbed
Salatiel F <95117339+SalatielF@users.noreply.github.com>
2022-04-05 15:54:48 -0400

    salatiel

---
+++ sei la2.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	printf("Digite um numero: ");
+	scanf("%d", &n);
+	
+	if(n>0){
+		printf("%d e positvo", n);
+		
+		if(n<0){
+			printf("%d e negativo", n);
+		}else{
+			printf("%d e neutro", n);
+		}
+	}
+	
+	
+return 0;
+}
